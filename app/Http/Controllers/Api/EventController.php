00c0d64960d68d4791de5bbec47f3d8d7209b64c
luisescx <luis.esculapio@live.com>
2023-08-21 18:57:59 -0300

    feat: add event and attendee policies

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -17,6 +17,7 @@ class EventController extends Controller
     public function __construct()
     {
         $this->middleware('auth:sanctum')->except(['index', 'show']);
+        $this->authorizeResource(Event::class, 'event');
     }
 
     /**
@@ -61,9 +62,9 @@ class EventController extends Controller
      */
     public function update(Request $request,  Event $event)
     {
-        if (Gate::denies('update-event', $event)) {
-          abort(403, 'You are not authorized to update this event.');
-        }
+        // if (Gate::denies('update-event', $event)) {
+        //   abort(403, 'You are not authorized to update this event.');
+        // }
 
         $event->update([
           ...$request->validate([
