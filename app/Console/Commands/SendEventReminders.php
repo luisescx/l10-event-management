87a70be36968a1b25c19278c5d87a6644dc3ebdd
luisescx <luis.esculapio@live.com>
2023-08-29 18:09:17 -0300

    feat: finish project

--- app/Console/Commands/SendEventReminders.php
+++ app/Console/Commands/SendEventReminders.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Event;
+use App\Notifications\EventReminderNotification;
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
 
@@ -39,7 +40,12 @@ class SendEventReminders extends Command
 
         $events->each(
             fn($event) => $event->attendees->each(
-                fn($attendee) => $this->info("Notifying the user {$attendee->user->id} about the event {$event->id}")
+                // fn($attendee) => $this->info("Notifying the user {$attendee->user->id} about the event {$event->id}")
+                fn($attendee) => $attendee->user->notify(
+                  new EventReminderNotification(
+                      $event
+                  )
+                )
             )
         );
 
